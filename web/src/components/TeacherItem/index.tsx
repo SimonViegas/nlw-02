0cbae0c2cd6a882fa27f31f73ee09f5b694fa4a1
Simon Viegas <simon.viegas@outlook.com.br>
2020-08-04 16:08:04 -0300

    Componetização do TeachItem

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './style.css'
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+    <header>
+      <img src="https://avatars0.githubusercontent.com/u/33127367?s=460&v=4" alt="Simon Viegas"/>
+      <div>
+        <strong>Simon Viegas</strong>
+        <span>Java Script</span>
+      </div>
+    </header>
+    <p>
+      dadasd aa dadsad ada dsada daadasdadas da
+      <br /><br />
+      s adadad ad
+    </p>
+    <footer>
+      <p>
+        Preço/hora
+        <strong>R$80,00</strong>
+      </p>
+      <button type="button">
+        <img src={whatsappIcon} alt="Whatsapp"/>
+        Entrar em contato
+      </button>
+    </footer>
+    </article>
+  )
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem';
 
 import './style.css'
 
@@ -25,30 +25,10 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img src="https://avatars0.githubusercontent.com/u/33127367?s=460&v=4" alt="Simon Viegas"/>
-            <div>
-              <strong>Simon Viegas</strong>
-              <span>Java Script</span>
-            </div>
-          </header>
-          <p>
-            dadasd aa dadsad ada dsada daadasdadas da
-            <br /><br />
-            s adadad ad
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
+        <TeacherItem/>
       </main>
     </div>
   )
