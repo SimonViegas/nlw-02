e3f289ec9bb61f70b5346bdac78d5fa6a9cab054
Simon Viegas <simon.viegas@outlook.com.br>
2020-08-04 15:44:35 -0300

    Estilização da listagem para o TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import PageHeader from '../../components/PageHeader';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './style.css'
 
 function TeacherList() {
@@ -22,6 +23,33 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars0.githubusercontent.com/u/33127367?s=460&v=4" alt="Simon Viegas"/>
+            <div>
+              <strong>Simon Viegas</strong>
+              <span>Java Script</span>
+            </div>
+          </header>
+          <p>
+            dadasd aa dadsad ada dsada daadasdadas da
+            <br /><br />
+            s adadad ad
+          </p>
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }
