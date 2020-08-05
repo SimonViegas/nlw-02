51af878fd4fc4126a3e618b8fbfb1bde3bfefd14
Simon Viegas <simon.viegas@outlook.com.br>
2020-08-05 10:28:19 -0300

    Adicionado README.md

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  console.log('Acessou a rota "users"');
+  return response.send('Acessou a rota "users"');
+})
+
+app.listen(3333);
