d76b6012e8a9b72a017d5e89d3ca6a4c1adb23d0
Simon Viegas <simon.viegas@outlook.com.br>
2020-08-06 10:08:07 -0300

    Server: permitir acesso a API por outros domínios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors()); // permitir que aplicações de endereços diferentes acessem a API
 app.use(express.json());
 app.use(routes);
 
